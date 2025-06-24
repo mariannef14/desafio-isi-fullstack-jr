4cde86ea6865892b6c18dc8b435810e0c2ce7cdb
Marianne <mariannefelixs11@gmail.com>
2025-06-24 19:50:46 -0300

    feat: add novos tratamentos de excecao

--- src/main/java/com/isi/desafio_fullstack/config/exceptions/handler/ExceptionsHandler.java
+++ src/main/java/com/isi/desafio_fullstack/config/exceptions/handler/ExceptionsHandler.java
@@ -3,7 +3,10 @@ package com.isi.desafio_fullstack.config.exceptions.handler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.fge.jsonpatch.JsonPatchException;
 import com.isi.desafio_fullstack.config.exceptions.ApiError;
+import com.isi.desafio_fullstack.config.exceptions.CouponNotActiveException;
 import com.isi.desafio_fullstack.config.exceptions.ErrorsValidations;
+import com.isi.desafio_fullstack.config.exceptions.UnprocessableEntityException;
+import com.isi.desafio_fullstack.utils.UtilsCodes;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
+import java.time.DateTimeException;
 import java.util.stream.Collectors;
 
 
@@ -24,7 +27,7 @@ public class ExceptionsHandler {
 
         var formatError = ApiError
                             .builder()
-                            .timestamp(LocalDateTime.now())
+                            .timestamp(UtilsCodes.dateToday())
                             .code(HttpStatus.CONFLICT.value())
                             .status(HttpStatus.CONFLICT.name())
                             .message(exception.getMessage())
@@ -48,7 +51,7 @@ public class ExceptionsHandler {
 
         var formatError = ApiError
                             .builder()
-                            .timestamp(LocalDateTime.now())
+                            .timestamp(UtilsCodes.dateToday())
                             .code(HttpStatus.BAD_REQUEST.value())
                             .status(HttpStatus.BAD_REQUEST.name())
                             .message("Campos inválidos")
@@ -64,12 +67,12 @@ public class ExceptionsHandler {
     public ResponseEntity<ApiError> handlerErrorEntityNotFoundException(EntityNotFoundException exception){
 
         var formatError = ApiError
-                .builder()
-                .timestamp(LocalDateTime.now())
-                .code(HttpStatus.NOT_FOUND.value())
-                .status(HttpStatus.NOT_FOUND.name())
-                .message(exception.getMessage())
-                .build();
+                            .builder()
+                            .timestamp(UtilsCodes.dateToday())
+                            .code(HttpStatus.NOT_FOUND.value())
+                            .status(HttpStatus.NOT_FOUND.name())
+                            .message(exception.getMessage())
+                            .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(formatError);
 
@@ -83,14 +86,94 @@ public class ExceptionsHandler {
     public ResponseEntity<ApiError> handlerErrorJson(Exception exception){
 
         var formatError = ApiError
+                            .builder()
+                            .timestamp(UtilsCodes.dateToday())
+                            .code(HttpStatus.BAD_REQUEST.value())
+                            .status(HttpStatus.BAD_REQUEST.name())
+                            .message(exception.getMessage())
+                            .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(formatError);
+
+    }
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiError> handlerErrorIllegalArgumentException(IllegalArgumentException exception){
+
+        var formatError = ApiError
+                            .builder()
+                            .timestamp(UtilsCodes.dateToday())
+                            .code(HttpStatus.BAD_REQUEST.value())
+                            .status(HttpStatus.BAD_REQUEST.name())
+                            .message(exception.getMessage())
+                            .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(formatError);
+
+    }
+
+
+    @ExceptionHandler(UnprocessableEntityException.class)
+    public ResponseEntity<ApiError> handlerErrorUnprocessableEntityException(UnprocessableEntityException exception){
+
+        var formatError = ApiError
                 .builder()
-                .timestamp(LocalDateTime.now())
+                .timestamp(UtilsCodes.dateToday())
+                .code(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .status(HttpStatus.UNPROCESSABLE_ENTITY.name())
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(formatError);
+
+    }
+
+
+    @ExceptionHandler(DateTimeException.class)
+    public ResponseEntity<ApiError> handlerErrorDateTimeException(DateTimeException exception){
+
+        var formatError = ApiError
+                .builder()
+                .timestamp(UtilsCodes.dateToday())
                 .code(HttpStatus.BAD_REQUEST.value())
                 .status(HttpStatus.BAD_REQUEST.name())
                 .message(exception.getMessage())
                 .build();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(formatError);
+
+    }
+
+
+    @ExceptionHandler(CouponNotActiveException.class)
+    public ResponseEntity<ApiError> handlerErrorCouponNotActiveException(CouponNotActiveException exception){
+
+        var formatError = ApiError
+                .builder()
+                .timestamp(UtilsCodes.dateToday())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .status(HttpStatus.BAD_REQUEST.name())
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(formatError);
+
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handlerErrorException(Exception exception){
+
+        var formatError = ApiError
+                            .builder()
+                            .timestamp(UtilsCodes.dateToday())
+                            .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                            .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                            .message(exception.getMessage())
+                            .build();
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(formatError);
 
     }
 
